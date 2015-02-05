d5bd9d3198c240476f6af66c4363b761fbeefd37
YANG Jing <jyang@thoughtworks.com>
2015-02-05 13:52:22 +0800

    [JingYang] modify feedback from zzz.

--- generator/html.js
+++ generator/html.js
@@ -4,25 +4,25 @@ var cheerio = require('cheerio');
 (function ($) {
 	"use strict";
 	// your JavaScript code for generate DOM
-	$('body').append('<img class="" href="../source/logo.png"></img>');
-	$('body').append('<h4 class="left-corner" >ONLY JAVASCRIPT</h4>');
+	$('body').append('<img class="" href="../source/logo.jpg"></img>');
+	$('body').append('<h4 class="left-corner" >Only JavaScript</h4>');
 
 	$('body').append('<table class="right-corner" ></table>');
 	$('body').append('<tr class="right-corner" ></tr>');
 	$('.right-corner').append('<td>HOME </td>')
-	$('.right-corner').append('<td>OTHER </td>')
-	$('.right-corner').append('<td>ABOUT </td>')
+		.append('<td>OTHER </td>')
+		.append('<td>ABOUT </td>');
 	
 	$('body').append('<div id="title" ></div>');
-	$('#title').append('<h2 class="title">ONLY USE JAVASCRIPT TO</h2>'); //starting for example
-	$('#title').append('<h2 class="title">CAEATE THE WORLD</h2>');
+	$('#title').append('<h2 class="title">Only Use JavaScript To</h2>'); //starting for example
+	$('#title').append('<h2 class="title">Create The World</h2>');
 	$('body').append('<table class="tabContainer"></table>');
 	$('.tabContainer').append('<tr class="row"></tr>');
-	$('.row').append('<td class="col">CHEARJS</td>');
-	$('.row').append('<td class="col">ABSURDJS</td>');
-	$('.row').append('<td class="col">VANILLAJS</td>');
-	$('.row').append('<td class="col">JASMINE</td>');
-	$('.row').append('<td class="col">PIONEERJS</td>');
-	$('.row').append('<td class="col">GRUNTJS</td>');
+	$('.row').append('<td class="col">CHEARJS</td>')
+		.append('<td class="col">ABSURDJS</td>')
+		.append('<td class="col">VANILLAJS</td>')
+		.append('<td class="col">JASMINE</td>')
+		.append('<td class="col">PIONEERJS</td>')
+		.append('<td class="col">GRUNTJS</td>');
 	return $.html();
 })(cheerio.load('<body></body>'));
