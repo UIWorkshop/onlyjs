efeabf5934f44a6904245a0ebdc1d9a46feafabf
BruceZhang <jwbzhang@thoughtworks.com>
2015-02-05 13:22:55 +0800

    display html first

--- generator/html.js
+++ generator/html.js
@@ -3,8 +3,12 @@ var cheerio = require('cheerio');
 var importfiles = '<link rel="stylesheet" href="style.css">';
 (function ($) {
 	"use strict";
-	// your JavaScript code for generate DOM
-	$('body').append('<div></div>'); //starting for example
-
-	return $.html();
+	var ul = $('ul').append('<ul></ul>');
+	var li = $('li').append('<li></li>');
+	
+	$('h2').addclass('Head')
+	var main_title = $('h2.title').text('ONLY USE JAVASCRIPT TO CREATE THE WORLD!');
+	var body = main_title
+	var html = header
+	return $.html(header, body);
 })(cheerio.load('<head>' + importfiles + '</head><body></body>'));
