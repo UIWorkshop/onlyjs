d28f9327021773f8a02053e55fc2e9b6cd85f514
Lin Liu <linliu@thoughtworks.com>
2015-02-10 10:13:26 +0800

    [LL] - remove code

--- generator/html.js
+++ generator/html.js
@@ -70,41 +70,10 @@ var cheerio = require('cheerio');
 		$('.content').append($optCenter);
 	}	
 
-
 	createHtmlConstruct();
 	createOptLeft();
 	createOptRight();
 	createMainContent(); 
-
-
-	// $('header').append('<div class="opt-left"></div>');
-	// $('header', 'div').addClass('opt-left');
-	// $('.opt-left').append('<img src="../source/Logo.jpg"></img>');
-
-	// $('.opt-left').append('<h1>only JavaScript</h1>');
-
-	// $('header').append('<div class="opt-right"></div>');
-	// $('.opt-right').append('<nav class="header-nav"></nav>');
-
-	// $('.header-nav').append('<button>HOME</button>');
-	// $('.header-nav').append('<button>OTHER</button>');
-	// $('.header-nav').append('<button>ABOUT</button>');
-
-
-
-	// $('.content').append('<div class="container"></div>');
-	// $('.container').append('<h2>only use JavaScript create the world</h2>');
-	// $('.container').append('<nav class="center-nav"></nav>');
-
-	// $('.center-nav').append('<button>CheerioJS</button>');
-	// $('.center-nav').append('<button>AbsurdJS</button>');
-	// $('.center-nav').append('<button>VanillaJS</button>');
-	// $('.center-nav').append('<button>Jasmine</button>');	
-	// $('.center-nav').append('<button>PioneerJS</button>');
-	// $('.center-nav').append('<button>GruntJS</button>');
-
-
-
 
 	return $.html();
 })(cheerio.load('<body></body>'));
